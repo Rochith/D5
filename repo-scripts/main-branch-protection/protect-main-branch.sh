cf6cc3af76502364664cf81aa98a97c00d822767
Rochith <40782583+Rochith@users.noreply.github.com>
2022-10-21 15:24:54 +0530

    Create protect-main-branch.sh

---
+++ repo-scripts/main-branch-protection/protect-main-branch.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+
+export org="Rochith"
+export user="Rochith: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"
+export branch="main" 
+
+declare -a repositories=(
+    "D5"
+)
+
+JSON=$(cat << 'EOF'
+    {
+        "required_status_checks": null,
+        "enforce_admins": true,
+        "required_pull_request_reviews": {
+            "required_approving_review_count": 0,
+            "require_code_owner_reviews": true
+        },
+        "restrictions": null
+    }
+EOF
+)
+
+for repository_name in "${repositories[@]}"; do
+    echo "========"
+    echo "=======> $repository_name"
+    echo "========"
+        
+    curl \
+        --silent \
+        --user $user \
+        --header "Accept: application/vnd.github.v3+json" \
+        --request PUT \
+        https://api.github.com/repos/$org/$repository_name/branches/$branch/protection \
+        --data "$JSON"
+
+done
